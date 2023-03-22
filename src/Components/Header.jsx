8780f795429f275d745d8c6203bb4a66642d8561
bispo-daniel <dbispo.work@gmail.com>
2023-03-22 19:39:01 -0300

    commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,8 +12,8 @@ class Header extends Component {
                 <img src={logo} className="appLogo" alt="logo" />
                 
                     <div className="social">
-                        <a href="https://www.linkedin.com/in/bispo-daniel" target="_blank" rel="noreferrer"><img src={logoLinkedin} className="linkedinLogo" alt="linkedin" /></a>
-                        <a href="https://www.github.com/bispo-daniel" target="_blank" rel="noreferrer"><img src={logoGithub} className="githubLogo" alt="git hub" /></a>
+                        <a href="#" target="_blank" rel="noreferrer"><img src={logoLinkedin} className="linkedinLogo" alt="linkedin" /></a>
+                        <a href="#" target="_blank" rel="noreferrer"><img src={logoGithub} className="githubLogo" alt="git hub" /></a>
                     </div>
             </header>
         )
